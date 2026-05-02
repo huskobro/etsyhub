5bbb4bb4eb159226edc272beb1dbca5494734850
huskobro <huskobro@gmail.com>
2026-05-02 17:52:39 +0300

    Revert "feat(phase9): Listing handoff foundation (Task 1-3)"
    This reverts commit de7e503fcedbfc512a53459fc546f9f83502afbd.

--- prisma/migrations/20260502144447_phase9_listing_extend/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Listing" ADD COLUMN     "mockupJobId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Listing" ADD CONSTRAINT "Listing_mockupJobId_fkey" FOREIGN KEY ("mockupJobId") REFERENCES "MockupJob"("id") ON DELETE SET NULL ON UPDATE CASCADE;
